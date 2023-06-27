b7c62f957fb77f0f6a2425ad8b985a1f92d2051a
“Xuxx” <1483069944@qq.com>
2023-06-27 18:37:31 +0800

    登录界面

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,17 @@
+package com.itheima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@Slf4j
+@SpringBootApplication
+//在SpringBoot项目中, 在引导类/配置类上加了该注解后, 会自动扫描项目中(当前包及其子包下)的@WebServlet , @WebFilter , @WebListener 注解, 自动注册Servlet的相关组件 ;
+@ServletComponentScan
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功......");
+    }
+}
